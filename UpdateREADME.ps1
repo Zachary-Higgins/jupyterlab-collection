b7457d66d263b6143ebdca11bd219537c6632af1
Higgins, Zachary <zachhiggins+1@gmail.com>
2023-09-08 17:36:08 -0400

    Updated toc

--- UpdateREADME.ps1
+++ UpdateREADME.ps1
@@ -31,17 +31,21 @@ foreach($nb in $nbs)
 
     $path = ($nb | Resolve-Path -Relative).replace(".\","").replace("\","/")
 
-    $new = "|<font size = `"1`">" + $subject +"</font>|<font size = `"1`">" + $title + "</font>|<font size = `"1`">" + $desc + "...</font>|<font size = `"1`">[" + $path + "](" + $path + ")</font>|`r"
+    $new = "|" + $subject +"|" + $title + "|" + $desc + "...|[" + $path + "](" + $path + ")|`r"
 
     $elements += $new
 }
 
 
-$pattern = "## Table of contents:"
+$pattern_end = "## Table of contents:"
 $readme = (get-content README.md)
+$end_pos = $readme.IndexOf($pattern_end)
+$readme = $readme[0..$end_pos] -join "`r"
 
-$start_pos = $readme.IndexOf($pattern)
-$new_readme = $readme[0..$start_pos]
-$new_readme += $elements
+$new_readme = "$readme
+
+<div class=`"table-container`">
+$elements
+</div>"
 
 $new_readme | Out-File README.md -Force
